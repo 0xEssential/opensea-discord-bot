9f76bdb3bca73ed2477ec1aa1499e749c51ab83d
Sam Bauch <sbauch@gmail.com>
2021-04-05 11:18:59 -0400

    sales and bids for pp

--- checkSales.ts
+++ checkSales.ts
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import Discord, { TextChannel } from 'discord.js';
 import fetch from 'node-fetch';
 import { ethers } from "ethers";
-import { parseISO } from 'date-fns'
 
 
 const discordBot = new Discord.Client();
@@ -20,7 +19,7 @@ const  discordSetup = async (): Promise<TextChannel> => {
   })
 }
 
-const buildMessage = (sale: any) => (
+const buildSaleMessage = (sale: any) => (
   new Discord.MessageEmbed()
 	.setColor('#0099ff')
 	.setTitle(sale.asset.name + ' sold!')
@@ -38,11 +37,48 @@ const buildMessage = (sale: any) => (
 	.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
 )
 
+const buildListedMessage = (sale: any) => (
+  new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle(sale?.asset?.name + ' listed for sale')
+	.setURL(sale?.asset?.permalink)
+	.setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sammybauch/discord-opensea')
+	.setThumbnail(sale?.asset?.collection?.image_url)
+	.addFields(
+		{ name: 'Name', value: sale?.asset?.name },
+		{ name: 'Amount', value: `${ethers.utils.formatEther(sale?.starting_price)}${ethers.constants.EtherSymbol}`},
+		{ name: 'Seller', value: sale?.seller?.address,  },
+	)
+  .setImage(sale?.asset?.image_url)
+	.setTimestamp(sale?.created_date) // unclear why this seems broken
+	.setFooter('Listed on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
+)
+
+const buildBidMessage = (sale: any) => (
+  new Discord.MessageEmbed()
+	.setColor('#0099ff')
+	.setTitle(sale.asset.name + ' received a bid')
+	.setURL(sale.asset.permalink)
+	.setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sammybauch/discord-opensea')
+	.setThumbnail(sale.asset.collection.image_url)
+	.addFields(
+		{ name: 'Name', value: sale.asset.name },
+		{ name: 'Amount', value: `${ethers.utils.formatEther(sale.bid_amount)}${ethers.constants.EtherSymbol}`},
+		{ name: 'Bidder', value: sale?.from_account?.address, },
+		{ name: 'Seller', value: sale?.owner?.address,  },
+	)
+  .setImage(sale.asset.image_url)
+	.setTimestamp(sale.created_date) // unclear why this seems broken
+	.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
+)
+
 async function main() {
   const channel = await discordSetup();
-  const hoursAgo = (Math.round(new Date().getTime() / 1000) - (3_600)); // in the last hour, run hourly?
+  const hoursAgo = (Math.round(new Date().getTime() / 1000) - (43200)); // in the last 10 minutes, run every 10 mins
   
-  const openSeaResponse = await fetch(
+
+  // SALES
+  const salesResponse = await fetch(
     "https://api.opensea.io/api/v1/events?" + new URLSearchParams({
       offset: '0',
       limit: '100',
@@ -54,11 +90,50 @@ async function main() {
   })).then((resp) => resp.json());
 
   await Promise.all(
-    openSeaResponse?.asset_events?.map(async (sale: any) => {
-      const message = buildMessage(sale);
+    salesResponse?.asset_events?.map(async (sale: any) => {
+      const message = buildSaleMessage(sale);
       return channel.send(message)
     })
   );   
+
+
+  // LISTINGS
+  const listingsResponse = await fetch(
+    "https://api.opensea.io/api/v1/events?" + new URLSearchParams({
+      offset: '0',
+      limit: '100',
+      event_type: 'created',
+      only_opensea: 'true',
+      occurred_after: hoursAgo.toString(), 
+      collection_slug: process.env.COLLECTION_SLUG!,
+      contract_address: process.env.CONTRACT_ADDRESS!
+  })).then((resp) => resp.json());
+
+  await Promise.all(
+    listingsResponse?.asset_events?.map(async (sale: any) => {
+      const message = buildListedMessage(sale);
+      return channel.send(message)
+    })
+  );   
+
+    // BIDS
+    const bidsResponse = await fetch(
+      "https://api.opensea.io/api/v1/events?" + new URLSearchParams({
+        offset: '0',
+        limit: '100',
+        event_type: 'bid_entered',
+        only_opensea: 'true',
+        occurred_after: hoursAgo.toString(), 
+        collection_slug: process.env.COLLECTION_SLUG!,
+        contract_address: process.env.CONTRACT_ADDRESS!
+    })).then((resp) => resp.json());
+  
+    await Promise.all(
+      bidsResponse?.asset_events?.map(async (sale: any) => {
+        const message = buildBidMessage(sale);
+        return channel.send(message)
+      })
+    );   
 }
 
 main()
