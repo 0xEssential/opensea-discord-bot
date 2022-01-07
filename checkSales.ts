904de11f140bb12399c3d512c0db2133a2e3cb28
Max Veytsman <maxim@ontoillogical.com>
2022-01-07 11:18:51 -0500

    Added MockChannel to log when there's no discord

--- checkSales.ts
+++ checkSales.ts
@@ -1,17 +1,26 @@
 import 'dotenv/config';
-import Discord, { TextChannel } from 'discord.js';
+import Discord, { TextChannel, Message } from 'discord.js';
 import fetch from 'node-fetch';
 import { ethers } from "ethers";
 
 const OPENSEA_SHARED_STOREFRONT_ADDRESS = '0x495f947276749Ce646f68AC8c248420045cb7b5e';
 
 const discordBot = new Discord.Client();
-const  discordSetup = async (): Promise<TextChannel> => {
-  return new Promise<TextChannel>((resolve, reject) => {
-    ['DISCORD_BOT_TOKEN', 'DISCORD_CHANNEL_ID'].forEach((envVar) => {
-      if (!process.env[envVar]) reject(`${envVar} not set`)
-    })
-  
+
+class MockChannel {
+  send(message) {
+    console.log(message);
+  }
+}
+
+const discordSetup = async (): Promise<TextChannel | MockChannel> => {
+
+  return new Promise<TextChannel | MockChannel>((resolve, _reject) => {
+    if (!process.env['DISCORD_BOT_TOKEN'] || !process.env['DISCORD_CHANNEL_ID']) {
+      console.warn(`Discord API keys not set. Logging instead`)
+      return resolve(new MockChannel());
+    }
+
     discordBot.login(process.env.DISCORD_BOT_TOKEN);
     discordBot.on('ready', async () => {
       const channel = await discordBot.channels.fetch(process.env.DISCORD_CHANNEL_ID!);
@@ -22,32 +31,32 @@ const  discordSetup = async (): Promise<TextChannel> => {
 
 const buildMessage = (sale: any) => (
   new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle(sale.asset.name + ' sold!')
-	.setURL(sale.asset.permalink)
-	.setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sbauch/opensea-discord-bot')
-	.setThumbnail(sale.asset.collection.image_url)
-	.addFields(
-		{ name: 'Name', value: sale.asset.name },
-		{ name: 'Amount', value: `${ethers.utils.formatEther(sale.total_price || '0')}${ethers.constants.EtherSymbol}`},
-		{ name: 'Buyer', value: sale?.winner_account?.address, },
-		{ name: 'Seller', value: sale?.seller?.address,  },
-	)
-  .setImage(sale.asset.image_url)
-	.setTimestamp(Date.parse(`${sale?.created_date}Z`))
-	.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
+    .setColor('#0099ff')
+    .setTitle(sale.asset.name + ' sold!')
+    .setURL(sale.asset.permalink)
+    .setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sbauch/opensea-discord-bot')
+    .setThumbnail(sale.asset.collection.image_url)
+    .addFields(
+      { name: 'Name', value: sale.asset.name },
+      { name: 'Amount', value: `${ethers.utils.formatEther(sale.total_price || '0')}${ethers.constants.EtherSymbol}` },
+      { name: 'Buyer', value: sale?.winner_account?.address, },
+      { name: 'Seller', value: sale?.seller?.address, },
+    )
+    .setImage(sale.asset.image_url)
+    .setTimestamp(Date.parse(`${sale?.created_date}Z`))
+    .setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
 )
 
 async function main() {
   const channel = await discordSetup();
   const seconds = process.env.SECONDS ? parseInt(process.env.SECONDS) : 3_600;
   const hoursAgo = (Math.round(new Date().getTime() / 1000) - (seconds)); // in the last hour, run hourly?
-  
+
   const params = new URLSearchParams({
     offset: '0',
     event_type: 'successful',
     only_opensea: 'false',
-    occurred_after: hoursAgo.toString(), 
+    occurred_after: hoursAgo.toString(),
     collection_slug: process.env.COLLECTION_SLUG!,
   })
 
@@ -58,7 +67,7 @@ async function main() {
   let opts = {};
   console.log(process.env.OPENSEA_API_TOKEN)
   if (process.env.OPENSEA_API_TOKEN) {
-    opts["headers"] = {"X-API-KEY": process.env.OPENSEA_API_TOKEN }
+    opts["headers"] = { "X-API-KEY": process.env.OPENSEA_API_TOKEN }
   }
 
   const openSeaResponse = await fetch(
@@ -69,11 +78,11 @@ async function main() {
       const message = buildMessage(sale);
       return channel.send(message)
     })
-  );   
+  );
 }
 
 main()
-  .then((res) =>{ 
+  .then((res) => {
     if (!res.length) console.log("No recent sales")
     process.exit(0)
   })
