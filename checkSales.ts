bed32fee223be38e44f513892c7ec9bc1fc9f2a5
dcsan <dc@rik.ai>
2022-02-28 00:28:30 -0800

    changes to make it easier to work with - runs on fixed interval - moved some hardwired values to a config.js file - refactored functions a bit to be smaller chunks

--- checkSales.ts
+++ checkSales.ts
@@ -2,104 +2,135 @@ import 'dotenv/config';
 import Discord, { TextChannel } from 'discord.js';
 import fetch from 'node-fetch';
 import { ethers } from "ethers";
+import { config } from './config'
+import dayjs from 'dayjs'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
 
-const OPENSEA_SHARED_STOREFRONT_ADDRESS = '0x495f947276749Ce646f68AC8c248420045cb7b5e';
+dayjs.extend(localizedFormat)
+const clog = console
 
 const discordBot = new Discord.Client();
-const  discordSetup = async (channel: string): Promise<TextChannel> => {
-  const channelID = channel
-  return new Promise<TextChannel>((resolve, reject) => {
-    if (!process.env['DISCORD_BOT_TOKEN']) reject('DISCORD_BOT_TOKEN not set')
-    discordBot.login(process.env.DISCORD_BOT_TOKEN);
-    discordBot.on('ready', async () => {
-      const channel = await discordBot.channels.fetch(channelID!);
-      resolve(channel as TextChannel);
-    });
-  })
+
+const minutes = 5
+// run interval in seconds
+const runIntervalSeconds = minutes * 60
+
+function sleepSeconds(seconds: number) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
+
+const discordSetup = async (channel: string): Promise<TextChannel> => {
+    const channelID = channel
+    return new Promise<TextChannel>((resolve, reject) => {
+        if (!process.env['DISCORD_BOT_TOKEN']) reject('DISCORD_BOT_TOKEN not set')
+        discordBot.login(process.env.DISCORD_BOT_TOKEN);
+        discordBot.on('ready', async () => {
+            const channel = await discordBot.channels.fetch(channelID!);
+            resolve(channel as TextChannel);
+        });
+    })
+}
+
+const formatPrice = (price: number) => {
+    return `${ethers.utils.formatEther(price || '0')}${ethers.constants.EtherSymbol}`
 }
 
 const buildMessage = (sale: any) => (
-  new Discord.MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle(sale.asset.name + ' sold!')
-	.setURL(sale.asset.permalink)
-	.setAuthor('OpenSea Bot', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png', 'https://github.com/sbauch/opensea-discord-bot')
-	.setThumbnail(sale.asset.collection.image_url)
-	.addFields(
-		{ name: 'Name', value: sale.asset.name },
-		{ name: 'Amount', value: `${ethers.utils.formatEther(sale.total_price || '0')}${ethers.constants.EtherSymbol}`},
-		{ name: 'Buyer', value: sale?.winner_account?.address, },
-		{ name: 'Seller', value: sale?.seller?.address,  },
-	)
-  .setImage(sale.asset.image_url)
-	.setTimestamp(Date.parse(`${sale?.created_date}Z`))
-	.setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
+    new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(sale.asset.name + ' sold!')
+        .setURL(sale.asset.permalink)
+        .setAuthor(config.author.name, config.author.icon, config.author.link)
+        .setThumbnail(sale.asset.collection.image_url)
+        .addFields(
+            { name: 'Name', value: sale.asset.name },
+            { name: 'Amount', value: formatPrice(sale.total_price) },
+            { name: 'Buyer', value: sale?.winner_account?.address, },
+            { name: 'Seller', value: sale?.seller?.address, },
+        )
+        .setImage(sale.asset.image_url)
+        .setTimestamp(Date.parse(`${sale?.created_date}Z`))
+        .setFooter('Sold on OpenSea', 'https://files.readme.io/566c72b-opensea-logomark-full-colored.png')
 )
 
-async function main() {
-  const seconds = process.env.SECONDS ? parseInt(process.env.SECONDS) : 3_600;
-  const hoursAgo = (Math.round(new Date().getTime() / 1000) - (seconds)); // in the last hour, run hourly?
-  
-  const params = new URLSearchParams({
-    offset: '0',
-    event_type: 'successful',
-    only_opensea: 'false',
-    occurred_after: hoursAgo.toString(), 
-    collection_slug: process.env.COLLECTION_SLUG!,
-  })
+function buildUri() {
 
-  if (process.env.CONTRACT_ADDRESS !== OPENSEA_SHARED_STOREFRONT_ADDRESS) {
-    params.append('asset_contract_address', process.env.CONTRACT_ADDRESS!)
-  }
+    const hoursAgo = (Math.round(new Date().getTime() / 1000) - (runIntervalSeconds));
 
-  let openSeaFetch = {}
-  if (process.env.OPENSEA_TOKEN) {
-    openSeaFetch['headers'] = {'X-API-KEY': process.env.OPENSEA_TOKEN}
-  }
+    const params = new URLSearchParams({
+        offset: '0',
+        event_type: 'successful',
+        only_opensea: 'false',
+        occurred_after: hoursAgo.toString(),
+        collection_slug: process.env.COLLECTION_SLUG!,
+    })
 
-  let responseText = "";
+    // if (process.env.CONTRACT_ADDRESS) {
+    //     params.append('asset_contract_address', process.env.CONTRACT_ADDRESS!)
+    // }
 
-  try {
-    const openSeaResponseObj = await fetch(
-      "https://api.opensea.io/api/v1/events?" + params, openSeaFetch
-    );
-
-    responseText = await openSeaResponseObj.text();
-
-    const openSeaResponse = JSON.parse(responseText);
-
-    return await Promise.all(
-      openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
-        
-        if (sale.asset.name == null) sale.asset.name = 'Unnamed NFT';
-
-        const message = buildMessage(sale);
-
-        return await Promise.all(
-          process.env.DISCORD_CHANNEL_ID.split(';').map(async (channel: string) => {
-            return await (await discordSetup(channel)).send(message)
-          })
-        );
-      })
-    );
-  } catch (e) {
-    
-    const payload = responseText || "";
-
-    if (payload.includes("cloudflare") && payload.includes("1020")) {
-      throw new Error("You are being rate-limited by OpenSea. Please retrieve an OpenSea API token here: https://docs.opensea.io/reference/request-an-api-key")
+    let headers = {}
+    if (process.env.OPENSEA_TOKEN) {
+        headers['headers'] = { 'X-API-KEY': process.env.OPENSEA_TOKEN }
     }
-    
-    throw e;
-  }
+
+    let uri = "https://api.opensea.io/api/v1/events?" + params
+    // uri += '&occurred_after=' + hoursAgo.toString()
+    // clog.log('uri', uri)
+    return { uri, headers }
 }
 
+async function check(channel: any) {
+
+    const { uri, headers } = buildUri()
+    let responseText = "";
+    clog.log('running at:', dayjs().format('llll'))
+
+    try {
+        const openSeaResponseObj = await fetch(uri, headers)
+        responseText = await openSeaResponseObj.text()
+        const openSeaResponse = JSON.parse(responseText);
+        clog.log('items:', openSeaResponse.asset_events.length)
+
+        await Promise.all(
+            openSeaResponse?.asset_events?.reverse().map(async (sale: any) => {
+                clog.log('sale', formatPrice(sale.total_price))
+                if (sale.asset) {
+                    sale.asset?.name == sale.asset?.name || 'Unnamed NFT'
+                    const message = buildMessage(sale)
+                    try {
+                        await channel.send(message)
+                    } catch (err) {
+                        clog.warn('discord error', err)
+                    }
+                }
+            })
+        );
+    } catch (e) {
+        const payload = responseText || "";
+        if (payload.includes("cloudflare") && payload.includes("1020")) {
+            throw new Error("You are being rate-limited by OpenSea. Please retrieve an OpenSea API token here: https://docs.opensea.io/reference/request-an-api-key")
+        }
+        throw e;
+    }
+}
+
+async function main() {
+    const channel = await discordSetup(process.env.DISCORD_CHANNEL_ID)
+
+    while (true) {
+        await check(channel)
+        await sleepSeconds(runIntervalSeconds)
+    }
+
+}
+
+
 main()
-  .then((res) =>{ 
-    if (!res.length) console.log("No recent sales")
-    process.exit(0)
-  })
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then((res) => {
+        process.exit(0)
+    })
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
